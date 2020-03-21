bcda8e4d4fa13c78a239bbef9b1742bb8f1a77dd
Olexander <kravchenko.sania@gmail.com>
2020-03-21 14:05:08 +0200

    deploy

---
+++ precache-manifest.f6699549a9585a0c61cc6fe6ba4b2700.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d861502879b82c2ee4e",
+    "url": "/my-project/css/app.6b7fd76e.css"
+  },
+  {
+    "revision": "a8145fcfbc2979cfde91",
+    "url": "/my-project/css/chunk-75d02754.c1e76d4b.css"
+  },
+  {
+    "revision": "285218ffe48d10c64fd442e77b7f00bb",
+    "url": "/my-project/index.html"
+  },
+  {
+    "revision": "0d861502879b82c2ee4e",
+    "url": "/my-project/js/app.634952fa.js"
+  },
+  {
+    "revision": "a8145fcfbc2979cfde91",
+    "url": "/my-project/js/chunk-75d02754.33224f45.js"
+  },
+  {
+    "revision": "9046402793e103ae06ec",
+    "url": "/my-project/js/chunk-vendors.eaee70e0.js"
+  },
+  {
+    "revision": "555cd7e93bce8711aeaba4af24e86d3f",
+    "url": "/my-project/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/my-project/robots.txt"
+  }
+]);
